78096cc8ed9e8aa3ef970f10dfe2821e9fbf2a6d
ely211 <gonzalesm405@gmail.com>
2017-03-15 15:09:52 -0500

    modificación javascript

--- asstes/js/app.js
+++ asstes/js/app.js
@@ -5,37 +5,26 @@ function NewPatient (nombre,apellido,edad,genero,ciudad,pais){
   this.genero = genero;
   this.ciudad = ciudad;
   this.pais = pais;
-  this.allPatientData = [];
-  this.createDiv =  function(){
-                      var lista = document.getElementById("listPatients");
-                      var container = document.createElement("FIELDSET");
-                      var paragraphName = document.createElement("P");
-                      var conteName = document.createTextNode("Nombre: " + this.nombre);
-                      var paragraphLast = document.createElement("P");
-                      var conteLast = document.createTextNode("Apellido:" + this.apellido);
-                      var paragraphYearOld = document.createElement("P");
-                      var conteYearOld = document.createTextNode("Edad:" + this.edad);
-                      var paragraphGender = document.createElement("P");
-                      var conteGender = document.createTextNode("Género:" + this.genero);
-                      var paragraphCity = document.createElement("P");
-                      var conteCity = document.createTextNode("Ciudad:" + this.ciudad);
-                      var paragraphCountry = document.createElement("P");
-                      var conteCountry = document.createTextNode("País:" + this.pais);
-                      paragraphName.appendChild(conteName);
-                      paragraphLast.appendChild(conteLast);
-                      paragraphYearOld.appendChild(conteYearOld);
-                      paragraphGender.appendChild(conteGender);
-                      paragraphCity.appendChild(conteCity);
-                      paragraphCountry.appendChild(conteCountry);
-                      container.appendChild(paragraphName);
-                      container.appendChild(paragraphLast);
-                      container.appendChild(paragraphYearOld);
-                      container.appendChild(paragraphGender);
-                      container.appendChild(paragraphCity);
-                      container.appendChild(paragraphCountry);
-                      lista.appendChild(container);
-                    }
 }
+
+function createDiv(NewPatient){
+  var lista = document.getElementById("listPatients");
+
+    patientDataPrint.forEach(function(elem){
+      var containerPatient = document.createElement("FIELDSET");
+      containerPatient.setAttribute("class", "aPatient");
+      for(var prop in elem){
+        var paragraph = document.createElement("P");
+        var paragraphContent = document.createTextNode(prop +": " + elem[prop]);
+        paragraph.appendChild(paragraphContent);
+        containerPatient.appendChild(paragraph);
+      }
+      lista.appendChild(containerPatient);
+    })
+}
+
+var allPatientData = [];
+var patientDataPrint = [];
 
 window.addEventListener("load", function(){
   var valNom = document.getElementById("name");
@@ -44,11 +33,18 @@ window.addEventListener("load", function(){
   var genero = document.getElementById("selectGender");
   var ciudad = document.getElementById("city");
   var pais = document.getElementById("country");
-  document.getElementById("print").addEventListener("click", function(event){
+
+  document.getElementById("formPatientData").addEventListener("submit", function(event){
     event.preventDefault();
-      var registroNew = new NewPatient(valNom.value, lastName.value, edad.value, genero.value, ciudad.value, pais.value);
-      registroNew.createDiv();
-      document.getElementById("formPatientData").reset();
-      valNom.focus();
+  })
+
+  document.getElementById("print").addEventListener("click", function(){
+    var newRegister = new NewPatient(valNom.value, lastName.value, edad.value, genero.value, ciudad.value, pais.value);
+    allPatientData.push(newRegister);
+    patientDataPrint.push(newRegister);
+    createDiv(newRegister);
+    document.getElementById("formPatientData").reset();
+    valNom.focus();
+    patientDataPrint.shift();
   })
 })
